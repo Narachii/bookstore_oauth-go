bba9b62d5a11856e5c0f9eb28db64098a3b798ed
Narachii <naoya706oxford@gmail.com>
2021-08-14 00:37:34 +0900

    hotfix request url

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -97,7 +97,7 @@ func cleanRequest(request *http.Request) {
 }
 
 func getAccessToken(accessTokenId string) (*accessToken, *errors.RestErr) {
-	response := oauthRestClient.Get(fmt.Sprintf("/oauth/acess_token/%s", accessTokenId))
+	response := oauthRestClient.Get(fmt.Sprintf("/oauth/access_token/%s", accessTokenId))
 	if response == nil || response.Response == nil {
 		return nil, errors.NewInternalServerError("invalid restclient response when trying to get access token")
 	}
